53c15f1f982ce678649524cf98ba1e61eef6d313
Vythals <Vythals@users.noreply.github.com>
2023-01-27 08:58:26 -0500

    s1c2 Début de la création de notre thème

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+wp_enqueue_style(   'style-principale',
+                    get_template_directory_uri() . '/style.css',
+                    array(),
+                    filemtime(get_template_directory() . '/style.css')
+                );
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Modele par defaut
+ * 
+ */
+?>
+<?php get_header(); ?>
+<h1>Bien venue au cours de 4W4</h1>
+<?php get_footer(); ?>
