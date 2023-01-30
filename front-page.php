60bb73171ca091231fead95431ff076e39db5fae
Vythals <Vythals@users.noreply.github.com>
2023-01-30 13:43:56 -0500

    s2c1 stucture sass et model frontpage, signle

--- front-page.php
+++ front-page.php
@@ -6,11 +6,11 @@
 ?>
 <?php get_header(); ?>
 <main>
-<h3>index.php</h3>
+    <h3>front-page.php</h3>
     <?php   if(have_posts()): 
                 while (have_posts()) : the_post();
                 the_title('<h1>','</h1>');
-                the_content(); ?>
+                //the_content(); ?>
                     <hr>
                 <?php endwhile;
             endif;
