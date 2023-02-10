d8ca1e3fc97ebbd19bd9ebe8d45aec0471cca293
Vythals <Vythals@users.noreply.github.com>
2023-02-10 09:28:21 -0500

    s3c2 ajout des sous-titre aux liens des articles et formatage addaptatif pour le menu entete et le titre du site

--- front-page.php
+++ front-page.php
@@ -12,7 +12,16 @@
         while (have_posts()) : the_post(); ?>
         <h1>
             <a href="<?php echo get_permalink();?>"><?php echo get_the_title(); ?></a>
+            <br>
+            <small>
+            <?php //the_content(); // affiche le contenu complet de l'article?>
+            <?php //the_excerpt(); // affiche un résumé de l'article?>
+            <?= wp_trim_words(get_the_excerpt(),10," &#10148; ")?>
+            </small>
+            
         </h1>
+
+        
         <?php endwhile;
     endif;
 ?>
--- readme.md
+++ readme.md
@@ -1,11 +1,13 @@
-# Laboratoire 1
+# exercice 1
 ## 4w4 - Conception d'interface et développement Web
 ### Auteur: Vincent Hum
 #### Description
-- Instalation local de WordPress
-- Création d'un thème simple
+- formatage scss
+- Création d'un thème plus développé
 - Création d'un dépôt git local
+- 5 commit
 - Création d'un dépôt github
+- dépôt sur un serveur enligne
 
 #### Références
 - https://developer.wordpress.org/
