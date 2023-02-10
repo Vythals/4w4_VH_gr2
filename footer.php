0c087c28400e6f05c8268452f8831663b032a590
Vythals <Vythals@users.noreply.github.com>
2023-02-10 10:49:47 -0500

    s3c2 changement du titre h3 du body et ajout de contenu dans le footer

--- footer.php
+++ footer.php
@@ -1,7 +1,16 @@
-<footer class="site__footer">
-    <section>Colonne 1</section>
-    <section>Colonne 2</section>
-    <section>Colonne 3</section>
+<footer>
+    <h1>Footer-4W4</h1>
+    <div class="site__footer">
+        <section>Colonne 1
+        <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id molestiae eaque animi officia eius velit? Modi exercitationem reiciendis provident, porro laborum voluptatibus ullam consectetur, suscipit ipsa repellendus id quis animi.</small>
+        </section>
+        <section>Colonne 2 
+        <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id molestiae eaque animi officia eius velit? Modi exercitationem reiciendis provident, porro laborum voluptatibus ullam consectetur, suscipit ipsa repellendus id quis animi.</small>
+        </section>
+        <section>Colonne 3 
+        <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id molestiae eaque animi officia eius velit? Modi exercitationem reiciendis provident, porro laborum voluptatibus ullam consectetur, suscipit ipsa repellendus id quis animi.</small></section>
+    </div>
+    
 </footer>
 <?php wp_footer(); ?>
 </body>
--- front-page.php
+++ front-page.php
@@ -6,7 +6,7 @@
 ?>
 <?php get_header(); ?>
 <main>
-    <h3>front-page.php</h3>
+    <h3>Articles</h3>
 <?php
     if (have_posts()): 
         while (have_posts()) : the_post(); ?>
